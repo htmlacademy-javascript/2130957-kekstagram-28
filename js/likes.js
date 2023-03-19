8f6ac063e777afffca1bc0e96b7d24fd79316286
alex-kabanin <alexcobainius@gmail.com>
2023-03-20 00:07:16 +0400

    likes removed

--- js/likes.js
+++
@@ -1,9 +0,0 @@
-const like = document.querySelector('.likes-count');
-like.addEventListener('click', () => {
-  if (like.classList.contains('likes-count--active')) {
-    like.textContent--;
-  } else {
-    like.textContent++;
-  }
-  like.classList.toggle('likes-count--active');
-});
